0b84399164132bd035cf28ab45e9a80d99797d63
ChrisYuneva <christina_yuneva@mail.ru>
2022-12-04 18:18:17 +0400

    add bad code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  options: AnimationOptions = {
+    path: '../../assets/lottie.json',
+  };
   constructor() { }
 
   ngOnInit(): void {
