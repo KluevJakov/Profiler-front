04da1854ed1c4d3395fb5ca0a024227909032b43
ChrisYuneva <christina_yuneva@mail.ru>
2022-12-07 11:05:04 +0400

    fix styles

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,8 +1,10 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit, ɵisDefaultChangeDetectionStrategy } from '@angular/core';
-import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment';
-import { AuthService } from '../auth.service';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Component, OnInit, ɵisDefaultChangeDetectionStrategy} from '@angular/core';
+import {Router} from '@angular/router';
+import {environment} from 'src/environments/environment';
+import {AuthService} from '../auth.service';
+import {AnimationOptions} from 'ngx-lottie';
+
 
 const API_URL: string = environment.apiUrl;
 
@@ -20,9 +22,14 @@ export class ProfileComponent implements OnInit {
   allCategory: { id: number; name: string, token: { val: string }[] }[] = [];
   currentLogin = JSON.parse(AuthService.getCurrentUser()).login;
 
+  //for lottie animations
+  options: AnimationOptions = {
+    path: '../../assets/document.json',
+  };
+
   constructor(private http: HttpClient,
-    private authService: AuthService,
-    private router: Router) {
+              private authService: AuthService,
+              private router: Router) {
     this.http.get<any>(API_URL + '/api/doc/getCategories', AuthService.getJwtHeader())
       .subscribe(
         (result: any) => {
@@ -37,17 +44,17 @@ export class ProfileComponent implements OnInit {
   deleteCat(event: any, item: any): void {
     let catId = item;
     console.log(catId['id']);
-    
+
     this.http.get<any>(API_URL + "/api/doc/deleteCategory/" + catId['id'], AuthService.getJwtHeader())
       .subscribe(
         (result: any) => {
-          this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=> this.router.navigate(['/profile']));
+          this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => this.router.navigate(['/profile']));
         },
         (error: HttpErrorResponse) => {
-          this.router.navigateByUrl('/', {skipLocationChange: true}).then(()=> this.router.navigate(['/profile']));
+          this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => this.router.navigate(['/profile']));
         }
       );
-      
+
   }
 
   createCategory(): void {
